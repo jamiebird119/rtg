4faa5976f5f903b7da35f16b69838318fc550ab7
jamiebird119 <jamie.bird119@gmail.com>
2020-06-05 12:35:39 +0000

    NBA schedule successfully added to Games table on mysql

--- request.py
+++ request.py
@@ -2,7 +2,7 @@ import os
 import pymysql
 import requests
 import json
-import datetime
+from datetime import datetime
 
 response = requests.get(
     "http://api.sportradar.us/nba/trial/v7/en/games/2019/REG/schedule.json?api_key=qhjbwd99fwtzvpasey7ebj7t")
@@ -10,10 +10,10 @@ response = requests.get(
 data = json.loads(response.content.decode("utf-8"))
 schedule = []
 for i in data["games"]:
-    schedule.append({i["id"],
-                    i["home"]["id"],
-                    i["away"]["id"],
-                    i["scheduled"]})
+    schedule.append({"id": i["id"],
+                     "home": i["home"]["id"],
+                     "away": i["away"]["id"],
+                     "scheduled": i["scheduled"]})
 
 username = os.getenv('C9_USER')
 connection = pymysql.connect(host='localhost',
@@ -24,7 +24,14 @@ try:
     with connection.cursor(pymysql.cursors.DictCursor) as cursor:
 
         for i in schedule:
-            cursor.execute("INSERT INTO Games(GameId, Home_Team_Id, Away_Team_Id, Scheduled) Values ({})", i)
+            id = i["id"]
+            home = i["home"]
+            away = i["away"]
+            date = str.split(i["scheduled"], 'T')
+            date_f = date[0]
+            print (id,home,away,date_f)
+            cursor.execute(
+                f"INSERT INTO Games (`GameId`, `Home_Team_Id`, `Away_Team_Id`, `Scheduled`) Values ('{id}','{home}','{away}','{date_f}')")
     connection.commit()
 finally:
     connection.close()
